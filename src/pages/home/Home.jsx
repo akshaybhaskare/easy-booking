a1284bc6555aa518d9e22f41b5d8a87c5e2ef17b
akshay bhaskare <akshaybhaskare12@gmail.com>
2022-05-10 14:53:07 +0530

    property page huest love section done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import './home.css';
+import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 
 const Home = () => {
   return( 
@@ -12,6 +14,9 @@ const Home = () => {
    <div className="homeContainer">
      <Featured/>
      <h1 className="homeTitle">Browse by property type</h1>
+     <PropertyList/>
+     <h1 className="homeTitle">Home guest love</h1>
+     <FeaturedProperties/>
    </div>
    </div>
   );
